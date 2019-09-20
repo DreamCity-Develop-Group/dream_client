7145601595a14c48e519197b01ba8ba008bbc2e2
CachePeng <1448828696@qq.com>
2019-09-20 20:09:42 +0800

    多语言方案

---
+++ Assets/Language/LanguageText.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Language
+{
+	[RequireComponent(typeof (Text))]
+	[AddComponentMenu("Language/LanguageText")]
+
+	public class LanguageText : UIBase {
+		 public string Language;
+		 public string File;
+		 public string Key;
+		 public string Value;
+
+		public LanguageService Localization;
+        // Use this for initialization
+
+        private void Awake()
+        {
+            Bind(UIEvent.LANGUAGE_VIEW);
+        }
+
+        public override void Execute(int eventCode, object message)
+        {
+            switch (eventCode)
+            {
+                case UIEvent.LANGUAGE_VIEW:
+                     Language = message.ToString();
+                    SetTextLanguage(Language);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private void SetTextLanguage(string language)
+        {
+            Localization = LanguageService.Instance;
+            var label = GetComponent<Text>();
+            GetComponent<Text>().text = Localization.GetFromFile(File, Key, label.text);
+            string languagePath = "Localization/" + Language;
+            Debug.Log(languagePath);
+            Font t = Resources.Load<Font>(languagePath);
+
+            GetComponent<Text>().font = t;
+          //  Language = language;
+        }
+
+        void Start()
+		{
+			//Localization = LanguageService.Instance;
+			//var label = GetComponent<Text>();
+			//GetComponent<Text>().text = Localization.GetFromFile(File, Key, label.text);
+		}
+
+	}
+}
+
