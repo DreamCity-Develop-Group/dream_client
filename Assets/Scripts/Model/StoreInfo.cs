6f8ba01683039c9672817ae8350d08875b098912
CachePeng <1448828696@qq.com>
2019-09-24 21:24:49 +0800

    代码调整

---
+++ Assets/Scripts/Model/StoreInfo.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/***
+  * Title:     
+  *
+  * Created:	zp
+  *
+  * CreatTime:          2019/09/24 21:09:48
+  *
+  * Description:
+  *
+  * Version:    0.1
+  *
+  *
+***/
+public class StoreInfo 
+{
+    public   string id;
+    public  string 投资类型;
+    public string 投资名称;
+    public string 投资金额;
+    public string 预计收益;
+    public string 税收;
+    public string 释放条件;
+    public string 投资条件;
+
+}
